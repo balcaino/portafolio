46f4d357ad52d763d8b4969e4cb7fc9fc35ccaf4
balcaino <bo.alcaino@gmail.com>
2020-12-27 13:59:45 -0300

    subida final de portafolio

---
+++ assets/js/script.js
@@ -0,0 +1,11 @@
+$(function () {
+    $('[data-toggle="tooltip"]').tooltip();
+
+
+    $('#enviarCorreo').click(function (){
+        alert("El correo fue enviado correctamente...")
+    });
+
+
+   
+})
